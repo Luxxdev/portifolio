ac34db9f7bfbbd1d936b72c7e7bc1958bbe12d02
Luxx <lq.santos@hotmail.com>
2024-12-10 21:50:34 -0300

    home updated with new sections and placeholder image

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import Footer from '../../components/Footer/Footer'
 import NavBar from '../../components/NavBar/NavBar'
+import About from './sections/About/About'
 import Hero from './sections/Hero/Hero'
+import Projects from './sections/Projects/Projects'
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <About />
+      <Projects />
       <Footer />
     </>
   )
