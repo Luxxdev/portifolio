9b6c1502841ee2fa3f0e0a5b29274adb7cce8dd2
Luxx <lq.santos@hotmail.com>
2024-12-11 04:19:57 -0300

    footer icon links updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import { Container, Grid, styled, Typography } from '@mui/material'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import EmailIcon from '@mui/icons-material/Email'
 
 const Footer = () => {
   const StyledFooter = styled('div')(({ theme }) => ({
@@ -12,19 +11,29 @@ const Footer = () => {
     padding: '20px',
   }))
 
+  const StyledLink = styled('a')(({ theme }) => ({
+    // Adicione seus estilos aqui, por exemplo:
+    color: theme.palette.primary.contrastText,
+    textDecoration: 'none',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  }))
+
   return (
     <>
       <StyledFooter id="footer">
         <Container>
           <Grid container display={'flex'} alignItems={'center'} justifyContent={'center'} textAlign={'center'} pb={2}>
             <Grid item xs={12} md={4}>
-              <LinkedInIcon />
+              <StyledLink href="https://www.linkedin.com/in/lucasqueirolo/" target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon />
+              </StyledLink>
             </Grid>
             <Grid item xs={12} md={4}>
-              <GitHubIcon />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <EmailIcon />
+              <StyledLink href="https://github.com/Luxxdev" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon />
+              </StyledLink>
             </Grid>
           </Grid>
           <Typography textAlign={'center'}>© 2024 Lucas Queirolo - All rights reserved.</Typography>
