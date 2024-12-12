043aa11641000601c5bc1a0ade466f629cc0119a
Luxx <lq.santos@hotmail.com>
2024-12-11 23:15:51 -0300

    appearence

--- src/components/SkillsButton/SkillsButton.tsx
+++ src/components/SkillsButton/SkillsButton.tsx
@@ -7,12 +7,12 @@ interface SkillsButtonProps {
 const SkillsButton: React.FC<SkillsButtonProps> = ({ children }) => {
   const SkillsButton = styled('button')(() => ({
     backgroundColor: 'transparent',
-    border: `1px solid ${theme.palette.primary}`,
+    border: `1px solid ${theme.palette.primary.contrastText}`,
     borderRadius: '3px',
     padding: '15px 5px',
     margin: '20px',
     width: '100%',
-    color: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
     display: 'inline-flex',
     alignItens: 'center',
     justifyContent: 'center',
